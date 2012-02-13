92e7a7854cb152fc531c85014cf98382af4ed61b
Amine Bezzarga <abezzarg@gmail.com>
2012-02-13 01:27:00 +0100

    CRUD : read and delete works but not create and update yet

--- app.js
+++ app.js
@@ -52,7 +52,17 @@ app.configure('test', function() {
 
 // Routes
 
-app.get('/', routes.index);
+//app.get('/', routes.index);
+app.get('/', function(req, res) {
+  res.redirect('/documents')
+});
+
+// Error handling
+function NotFound(msg) {
+  this.name = 'NotFound';
+  Error.call(this, msg);
+  Error.captureStackTrace(this, arguments.callee);
+}
 
 // :format can be json or html
 
@@ -71,13 +81,6 @@ app.get('/documents', function(req, res) {
   });
 });
 
-app.get('/documents/:id.:format?/edit', function(req, res) {
-  Document.findById(req.params.id, function(d) {
-    res.render('documents/edit.jade', {
-      locals: { d: d }
-    });
-  });
-});
 
 app.get('/documents/new', function(req, res) {
   res.render('documents/new.jade', {
@@ -89,27 +92,94 @@ app.get('/documents/new', function(req, res) {
 app.get('/documents.:format?', routes.readDocs);
 
 // Create 
-app.post('/documents.:format?', routes.createDoc);
+//app.post('/documents.:format?', routes.createDoc){
+app.post('/documents.:format?', function(req, res) {
+  var d = new Document(req.body);
+  console.log("creating document: " + req.body + "vs " + d);
+  //d.user_id = req.currentUser.id;
+  d.save(function() {
+    switch (req.params.format) {
+      case 'json':
+        var data = d.toObject();
+        // TODO: Backbone requires 'id', but can I alias it?
+        data.id = data._id;
+        res.send(data);
+      break;
+
+      default:
+        //req.flash('info', 'Document created');
+        res.redirect('/documents');
+    }
+  });
+});
 
 // Read
-app.get('/documents/:id.:format?', function(req, res) {
+app.get('/documents/:id.:format?/edit', function(req, res) {
+  Document.findOne({ _id: req.params.id }, function(err, d) {
+    res.render('documents/edit.jade', {
+      locals: { d: d , title: 'Edit Document'}
+    });
+  });
 });
+
 
 // Update
 app.put('/documents/:id.:format?', function(req, res) {
+  // Load the document
+  Document.findOne({ _id: req.params.id }, function(err, d) {
+    // Do something with it
+    d.title = req.body.title;
+    d.data = req.body.data;
+    console.log("updating document: " + d.title);
+
+    // Persist the changes
+    d.save(function() {
+      // Respond according to the request format
+      switch (req.params.format) {
+        case 'json':
+          res.send(d.__doc);
+         break;
+
+         default:
+          res.redirect('/documents');
+      }
+    });
+  });
 });
 
 // Delete
-app.del('/documents/:id.:format?', function(req, res) {
+app.del('/documents/:id.:format?', function(req, res, next) {
+// Load the document
+  Document.findOne({ _id: req.params.id }, function(err, d) {
+  	//if (!d) return next(new NotFound('Document not found'));
+    // Do something with it
+    
+    //d.title = req.body.title;
+    //d.data = req.body.data;
+    console.log("deleting document: " + req.params.id);
+
+    // Persist the changes
+    d.remove(function() {
+      // Respond according to the request format
+      switch (req.params.format) {
+        case 'json':
+          res.send(d.__doc);
+         break;
+
+         default:
+          res.redirect('/documents');
+      }
+    });
+  });
 });
 
 
 
 
 
-//app.listen(3000);
-var port = process.env.PORT || 3000;
-app.listen(port, "0.0.0.0");
+app.listen(3000);
+//var port = process.env.PORT || 3000;
+//app.listen(port, "0.0.0.0");
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 
 exports.Document = Document;
